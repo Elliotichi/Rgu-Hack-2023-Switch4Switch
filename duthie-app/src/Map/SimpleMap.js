bb381d9ccec661d92d2bcc84da7486482e9fecfd
sraaaaaaay <srbroch@proton.me>
2024-02-24 21:00:40 +0000

    fix bad merge

--- duthie-app/src/Map/SimpleMap.js
+++ duthie-app/src/Map/SimpleMap.js
@@ -6,32 +6,27 @@ import {icon} from 'leaflet'
 
 
 const SimpleMap = () => {
-  const mapRef = useRef(null);
-  const latitude = 57.13149074585778;
-  const longitude = -2.1035564224553425;
+    const mapRef = useRef(null);
+    const latitude = 57.13049074585778;
+    const longitude = -2.1035564224553425;
+  
+    return ( 
+      // Make sure you set the height and width of the map container otherwise the map won't show
+        <MapContainer center={[latitude, longitude]} zoom={17} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* Additional map layers or components can be added here */}
+        <Marker position={[latitude, longitude]} icon={new icon({iconUrl:markericonpng, iconsize:[25,41], iconanchor:[12, 41]})}>
+          
+          <Popup>
+            WELCOME TO DUTHIE PARK o/ <br />
+          </Popup>
+        </Marker>
 
-  return (
-    // Make sure you set the height and width of the map container otherwise the map won't show
-    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{ height: "100vh", width: "100vw" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* Additional map layers or components can be added here */}
-
-      <Marker position={[latitude, longitude]}
-        eventHandlers={{
-          click: (e) => {
-            console.log('marker clicked', e)
-          },
-        }}>
-        <Popup>
-          WELCOME TO DUTHIE PARK o/ <br />
-        </Popup>
-      </Marker>
-
-    </MapContainer>
-  );
-};
-
-export default SimpleMap;
+        </MapContainer>
+    );
+  };
+  
+  export default SimpleMap;
