27118ec78f860f61e1d6a53a93f0748f561a530a
Zuzanna <zuzanna.ponia@gmail.com>
2024-02-24 15:54:43 +0000

    react example

--- duthie-app/src/App.js
+++ duthie-app/src/App.js
@@ -1,7 +1,20 @@
+import React, { useState } from 'react';
 
-function App() {
+function Hello() {
     return (
         <p>Hello world!</p>
+    );
+}
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <Hello />
+            <button onClick={() => setCount(count + 1)}>click to increment</button>
+            <p>{count}</p>
+        </div>
     );
 }
 
