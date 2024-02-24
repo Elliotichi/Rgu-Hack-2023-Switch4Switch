5c0e2c90a8c0a92008df242d9d08c773e5eab9f6
sraaaaaaay <srbroch@proton.me>
2024-02-24 20:23:54 +0000

    Working version

--- duthie-app/src/App.js
+++ duthie-app/src/App.js
@@ -7,8 +7,8 @@ function Hello() {
     );
 }
 
-// export function App() {
-//     const [count, setCount] = useState(0);
+export function App() {
+     const [count, setCount] = useState(0);
 
     return (
         <div>
