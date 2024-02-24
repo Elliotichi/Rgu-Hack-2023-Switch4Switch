b4ee67663640832ca890b1a4e1c60773bf740ac6
sraaaaaaay <srbroch@proton.me>
2024-02-24 23:52:04 +0000

    add image folder, json data

---
+++ duthie-app/src/data/points.js
@@ -0,0 +1,171 @@
+export const trail_points = [
+{
+    "id":1,
+    "name":"East Lodge", 
+    "description": "Nineteenth century baronial lodge in tooled coursed grey granite, built 1882/83.",
+    "image":"./images/1.png",
+    "lat":57.13230227267181,
+    "long:":-2.102209953529852
+}, 
+{
+    "id":2,
+    "name":"Playground Steam Roller", 
+    "description": "Colourful former working steam roller, now part of this children’s play area.",
+    "image":"./images/2.png",
+    "lat":57.131962743663415,
+    "long":-2.1022112946343614
+},
+{
+    "id":3,
+    "name":"The Gordon Highlanders Memorial", 
+    "description": "Nineteenth century baronial lodge in tooled coursed grey granite, built 1882/83.",
+    "image":"./images/3.png",
+    "lat":57.13168034632308,
+    "long":-2.1028442959618716
+},
+{
+    "id":4,
+    "name":"Hygeia", 
+    "description": "Designed by John Cassidy of Manchester and carved by Arthur Taylor in 1883. The monument comprises a standing figure of the Greek Goddess Hygeia (Health) in classical dress, carved in pale granite.",
+    "image":"./images/4.png",
+    "lat":57.13129604957494,
+    "long":-2.1015353779626134
+},
+{
+    "id":5,
+    "name":"Bowling Pavilion and Green", 
+    "description": "Early 20th century. The former Bowling Pavilion has been converted to provide park ranger facilities, whilst the green itself is used to hold community events.",
+    "image":"./images/5.png",
+    "lat":57.130772002113474,
+    "long":-2.100880918962979
+},
+{
+    "id":8,
+    "name":"Duthie Fountain", 
+    "description": "The Duthie Fountain was made by A Macdonald & Co. Aberdeen City Granite Works c.1883 of pink Peterhead granite. A two-tiered granite fountain set in large roll moulded basin.",
+    "image":"./images/8.png",
+    "lat":57.129843983201226,
+    "long":-2.1011089067292428
+},
+{
+    "id":9,
+    "name":"Boating Pond", 
+    "description": "The large pond has been used by the model yacht club since 1907.",
+    "image":"./images/9.png",
+    "lat":57.12926750797597,
+    "long":-2.101124999983332
+},
+{
+    "id":11,
+    "name":"Fountainhall Well", 
+    "description": "James Mackie and John Burnet, 1706. Small granite rubble cistern house built into hillside.",
+    "image":"./images/11.png",
+    "lat":57.12871213257799,
+    "long":-2.1040861587357647
+},
+{
+    "id":12,
+    "name":"McGrigor Obelisk", 
+    "description": "Alexander Ellis 1830-1917, Aberdeen. This tall obelisk is of red Peterhead granite, from the Stirlinghill quarry and built of large polished square blocks.",
+    "image":"./images/12.png",
+    "lat":57.12913139445596,
+    "long":-2.1037213783097415
+},
+{
+    "id":13,
+    "name":"World War II Loopholes", 
+    "description": "Loopholes Defensive loopholes dating from World War II.",
+    "image":"./images/13.png",
+    "lat":57.129078986980744,
+    "long": -2.107015130980024
+},
+{
+    "id":14,
+    "name":"The Iron Bridge", 
+    "description": "Circa 1883, three-span, flat-arched bridge.",
+    "image":"./images/14.png",
+    "lat":57.12904404862279,
+    "long":-2.105362890226862
+},
+{
+    "id":15,
+    "name":"The Linked Lakes", 
+    "description": "The three linked lakes were an important feature of the original design by William McKelvie.",
+    "image":"./images/15.png",
+    "lat":57.12979812753767,
+    "long":-2.105405805571096
+},
+{
+    "id":17,
+    "name":"Duthie Park Bandstand", 
+    "description": "A handsome, late Victorian, octagonal bandstand erected in c.1893 to a design by McDowall, Steven & Co. Glasgow and costing £400.",
+    "image":"./images/17.png",
+    "lat":57.130135856738235,
+    "long":-2.103254673941167
+},
+{
+    "id":18,
+    "name":"The West Lodge", 
+    "description": "The original West Lodge was removed due to the widening of Great Southern Road in 1938.",
+    "image":"./images/18.png",
+    "lat":57.13062206493901,
+    "long":-2.106537697775374
+},
+{
+    "id":19,
+    "name":"The Temperance Fountain", 
+    "description": "Polished granite temperance drinking fountain by James Hunter, 1883.",
+    "image":"./images/19.png",
+    "lat":57.13132661876702,
+    "long":-2.1059797983002793
+},
+{
+    "id":20,
+    "name":"The Gordon Highlanders Memorial", 
+    "description": "One of two memorials to the regiment in the park (see also 3).",
+    "image":"./images/20.png",
+    "lat":57.131312062005634,
+    "long":-2.107009766561991
+},
+{
+    "id":21,
+    "name":"The Mound", 
+    "description": "The mound is one of the original and most striking features in the park and unique within public parks in Scotland.",
+    "image":"./images/21.png",
+    "lat":57.13193508627749,
+    "long":-2.1062480192017667
+},
+{
+    "id":25,
+    "name":"Aberdeen City Coat of Arms", 
+    "description": "The right to bear a set of arms is a privilege granted by the sovereign through the Lord Lion King of Arms.",
+    "image":"./images/25.png",
+    "lat":57.131891416703354,
+    "long":-2.104193447096372
+},
+{
+    "id":31,
+    "name":"Gargoyle", 
+    "description": "Artist unknown. This water spouting gargoyle was formerly sited in the Victoria Park, Rosemount in a section known locally as Tom’s Castle.",
+    "image":"./images/31.png",
+    "lat":57.13226115088317, 
+    "long":-2.1046011428666325
+},
+{
+    "id":36,
+    "name":"Kelly’s Cats", 
+    "description": "Sydney Boyes (1878-1931), Southampton, England, 1910, cast iron painted black, Known locally as ‘Kelly’s Cats’, they are in fact leopards from the city coat of arms and were once ranged along the two parapets of Union Bridge.",
+    "image":"./images/36.png",
+    "lat":57.13235140039785, 
+    "long":-2.103538988096743
+},
+{
+    "id":41,
+    "name":"The Old Deeside Line", 
+    "description": "This former railway line, 1853-1966, is now a popular walking, cycling and horse riding route from the heart of Aberdeen to the countryside of Aberdeenshire.",
+    "image":"./images/41.png",
+    "lat":57.13107142541104, 
+    "long":-2.099276566429595
+},
+
+];
