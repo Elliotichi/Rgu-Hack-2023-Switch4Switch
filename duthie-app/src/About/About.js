82ad66223d997ec252a48c4fe96c79916960c1b7
Zuzanna <zuzanna.ponia@gmail.com>
2024-02-25 09:23:28 +0000

    accordion

--- duthie-app/src/About/About.js
+++ duthie-app/src/About/About.js
@@ -1,6 +1,8 @@
-import { Typography } from "@mui/material";
+import { Accordion, AccordionActions, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
 import React from "react";
 import { Header } from "../Navigation/Header";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Button } from "@mui/base";
 
 export function About() {
     return (
@@ -10,26 +12,82 @@ export function About() {
             </div>
             <div className="about">
                 <Typography variant="h6" className="center margin-bottom">Welcome to Duthie Park!</Typography>
-                <div className="about-img-container">
+                <div className="about-img-container margin-bottom">
                 <img src="./images/9.png" className="about-img"></img>
                 </div>
+                <div className="margin-bottom">
+                    <AccordionUsage />
+                </div>
+                <div className="margin-bottom">
+                <AccordionUsage2 />
+                </div>
+                <div>
+                    <AccordionUsage3 />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+ function AccordionUsage3() {
+    return (
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography>Address</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          <Typography className="aboutText">Duthie Park</Typography>
+          <Typography className="aboutText">    Polmuir Road</Typography>
+          <Typography className="aboutText">     Aberdeen</Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    );
+  }
+
+ function AccordionUsage2() {
+    return (
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography>Opening Times</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography className="aboutText" >Open 24 hours</Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    );
+  }
+function AccordionUsage() {
+    return (
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography>About!</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
                 <Typography className="margin-bottom aboutText">
                     Duthie Park opened in 1883 after it was gifted to the city by Miss Elizabeth Crombie Duthie in 1880 for the ‘wellbeing and recreation of Aberdeen inhabitants'.
                 </Typography>
                 <Typography className="margin-bottom aboutText">
                 Duthie Park is loved by locals and visitors due to its wonderfully restored Victorian features such as the band stand, fountains, and boating ponds. The park also has an astro turf area for ball games and two other play areas; one for younger children and another for the more adventurous child which has been voted best play space in Scotland. The Park Café is also very popular with visitors.
                 </Typography>
-                <Typography variant="h6" className="center margin-bottom">
-                    Opening times
-                </Typography>
-                <Typography className="aboutText" >Open 24 hours</Typography>
-                <Typography variant="h6" className="center margin-bottom">
-                    Address
-                </Typography>
-                <Typography className="aboutText">Duthie Park
-                Polmuir Road
-                Aberdeen</Typography>
-            </div>
-        </div>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     );
-}
+  }
